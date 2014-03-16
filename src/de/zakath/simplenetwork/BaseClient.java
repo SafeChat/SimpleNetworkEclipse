7af3ded9da09ce395a65311b9c2567748b1d49c0
Zakant <zakant@gmx.de>
2014-03-16 14:41:27 +0100

    Hinzufügen von JavaDoc Kommentaren

--- src/de/zakath/simplenetwork/BaseClient.java
+++ src/de/zakath/simplenetwork/BaseClient.java
@@ -34,6 +34,16 @@ public class BaseClient
 
 	private Exception _lastexception;
 
+	/**
+	 * Creates a new BaseClient Socket wrapper
+	 * 
+	 * @param host
+	 *            The host address the client should connect to
+	 * @param port
+	 *            the port the client should connect to
+	 * @param ID
+	 *            The client id the client sends to the server with each message
+	 */
 	public BaseClient(String host, int port, int ID)
 	{
 		_host = host;
@@ -44,6 +54,16 @@ public class BaseClient
 		_isserver = false;
 	}
 
+	/**
+	 * Creates a new BaseClient Socket wrapper based on an existing socket
+	 * 
+	 * @param s
+	 *            The already connected socket
+	 * @param ID
+	 *            The id send to the client/server with each message
+	 * @param isServer
+	 *            An boolean indicating if this instance runs on a server or not
+	 */
 	public BaseClient(Socket s, int ID, boolean isServer)
 	{
 		this.s = s;
@@ -65,41 +85,85 @@ public class BaseClient
 		}
 	}
 
+	/**
+	 * Returns the assigend ID for this BaseClient instance
+	 * 
+	 * @return The assigned ID
+	 */
 	public int getID()
 	{
 		return _id;
 	}
 
+	/**
+	 * Assigns an ID for this BaseClient instance
+	 * 
+	 * @param id
+	 *            The ID that should be assigned
+	 */
 	public void setID(int id)
 	{
 		_id = id;
 	}
 
+	/**
+	 * Returns if running on a server or not
+	 * 
+	 * @return The boolean representig the status
+	 */
 	public boolean isServer()
 	{
 		return _isserver;
 	}
 
+	/**
+	 * Returns if this instance has succesfull completed authication with the
+	 * Server
+	 * 
+	 * @return The boolean representing the status
+	 */
 	public boolean isLocked()
 	{
 		return _locked;
 	}
 
+	/**
+	 * Sets the loocke status with indicates if the authication has succesfull
+	 * be completed
+	 * 
+	 * @param b
+	 *            The booleanvalue wich should be set
+	 */
 	public void setLocked(boolean b)
 	{
 		_locked = b;
 	}
 
+	/**
+	 * Return if the client is connected or not
+	 * 
+	 * @return The boolean representing the status
+	 */
 	public boolean isConnected()
 	{
 		return s != null && s.isConnected();
 	}
 
+	/**
+	 * Returns if the client is shutdowned or not
+	 * 
+	 * @return The boolean representing the status
+	 */
 	public boolean isShutdown()
 	{
 		return s == null;
 	}
-	
+
+	/**
+	 * Connect the client to the , in the constructor, specifified host and port
+	 * 
+	 * @return Returns whether the connection attempt was succesfull or not
+	 */
 	public boolean connect()
 	{
 		try
@@ -122,6 +186,9 @@ public class BaseClient
 		}
 	}
 
+	/**
+	 * Start listening to the connection
+	 */
 	public void startListening()
 	{
 		_listenthread = new Thread(new Runnable()
@@ -151,6 +218,9 @@ public class BaseClient
 		_listenthread.start();
 	}
 
+	/**
+	 * Stop listening to the connection
+	 */
 	public void stopListenting()
 	{
 		if (_listenthread != null)
@@ -158,6 +228,14 @@ public class BaseClient
 
 	}
 
+	/**
+	 * Sends a message to the remote client
+	 * 
+	 * @param m
+	 *            The message which should be send
+	 * @return A boolean indicating if the message transfer was succesfull or
+	 *         not
+	 */
 	public boolean sendMessage(Message m)
 	{
 		try
@@ -175,6 +253,11 @@ public class BaseClient
 		}
 	}
 
+	/**
+	 * Shutdown the connection and the client
+	 * 
+	 * @return A boolean indicating if shutdown was succesfull or not
+	 */
 	public boolean shutdown()
 	{
 		stopListenting();
@@ -192,16 +275,31 @@ public class BaseClient
 		}
 	}
 
+	/**
+	 * Provieds access to the latest exception thrown by the client
+	 * 
+	 * @return The latest exception
+	 */
 	public Exception getLatestException()
 	{
 		return _lastexception;
 	}
 
+	/**
+	 * NIY
+	 * 
+	 * @return
+	 */
 	public boolean suspend()
 	{
 		return false;
 	}
 
+	/**
+	 * NIY
+	 * 
+	 * @return
+	 */
 	public boolean resume()
 	{
 		return false;
@@ -209,11 +307,22 @@ public class BaseClient
 
 	private final List<NewMessageListener> _newmsglisteners;
 
+	/**
+	 * Adds a new MessageListener to the MessageListenerlist
+	 * 
+	 * @param listener
+	 *            The listener that should be added
+	 */
 	public void addNewMessageListener(NewMessageListener listener)
 	{
 		_newmsglisteners.add(listener);
 	}
 
+	/**
+	 * Removes a MessageListener from the MessageListenerlist
+	 * 
+	 * @param listener The MessageListener that should be removed
+	 */
 	public void removeNewMessageListener(NewMessageListener listener)
 	{
 		_newmsglisteners.remove(listener);
@@ -229,11 +338,19 @@ public class BaseClient
 
 	private final List<ConnectionLostListener> _connectionlostlistener;
 
+	/**
+	 * Adds a new ConnectionLostListener to the ConnectionLostListenerlist
+	 * @param listener The ConnectionLostListener that should be add
+	 */
 	public void addConnectionLostListener(ConnectionLostListener listener)
 	{
 		_connectionlostlistener.add(listener);
 	}
 
+	/**
+	 * Removes a ConnectionLostListener from the ConnectionLostListenerlist
+	 * @param listener The ConnectionLostListener that should be removed
+	 */
 	public void removeConnectionLostListener(ConnectionLostListener listener)
 	{
 		_connectionlostlistener.remove(listener);
